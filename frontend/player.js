15f6db94a640e0353def2566c2c7def2570c1900
Benoite142 <benoit.charbonneau01@gmail.com>
2025-07-15 09:19:48 -0400

    add positions player stats

--- frontend/player.js
+++ frontend/player.js
@@ -15,7 +15,7 @@ fetch(`/stats?id=${playerId}`)
   ${data.position ? `<p><strong>Position:</strong> ${data.position}</p>` : ""}
   ${data.team ? `<p><strong>Team:</strong> ${data.team}</p>` : ""}
 `;
-  const seasonStats = getGoalieSeasonTotals(data.seasonTotals || []);
+  const seasonStats = data.position === "Goalie"? getGoalieSeasonTotals(data.seasonTotals || []): getPlayerSeasonTotals(data.seasonTotals || []);
 
   
     document.body.innerHTML = `
@@ -52,16 +52,16 @@ function getGoalieSeasonTotals(seasonTotals) {
       <td>${season.season}</td>
       <td>${season.teamName?.default || 'N/A'}</td>
       <td>${season.leagueAbbrev}</td>
-      <td>${season.gamesPlayed? season.gamesPlayed: '--'}</td>
-      <td>${season.wins? season.wins: '0'}</td>
-      <td>${season.losses? season.losses: '0'}</td>
-      <td>${season.otLosses? season.otLosses: '0'}</td>
-      <td>${season.shotsAgainst? season.shotsAgainst: '--'}</td>
-      <td>${season.goalsAgainst? season.goalsAgainst: '--'}</td>
-      <td>${season.goalsAgainstAvg? season.goalsAgainstAvg: '--'}</td>
-      <td>${season.savePctg? season.savePctg: '--'}</td>
-      <td>${season.shutouts? season.shutouts: '0'}</td>
-      <td>${season.timeOnIce? season.timeOnIce: '--'}</td>
+      <td>${season.gamesPlayed?? '--'}</td>
+      <td>${season.wins?? '--'}</td>
+      <td>${season.losses?? '--'}</td>
+      <td>${season.otLosses?? '--'}</td>
+      <td>${season.shotsAgainst?? '--'}</td>
+      <td>${season.goalsAgainst?? '--'}</td>
+      <td>${season.goalsAgainstAvg?? '--'}</td>
+      <td>${season.savePctg?? '--'}</td>
+      <td>${season.shutouts?? '--'}</td>
+      <td>${season.timeOnIce?? '--'}</td>
     </tr>
   `).join('');
 
@@ -82,6 +82,69 @@ function getGoalieSeasonTotals(seasonTotals) {
           <th>SV%</th>
           <th>SO</th>
           <th>TOI</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${rows}
+      </tbody>
+    </table>
+  `;
+}
+
+
+function getPlayerSeasonTotals(seasonTotals) {
+  if(!seasonTotals || seasonTotals.length === 0) return "<p>No season stats available.</p>";
+  
+  const rows = seasonTotals.map(season => {
+    let formatedSeason = season.season.toString().slice(2,4) +'-'+ season.season.toString().slice(6,8);
+  return `
+    <tr>
+      <td>${formatedSeason}</td>
+      <td>${season.leagueAbbrev?? "--"}</td>
+      <td>${season.teamName.default?? "--"}</td>
+      <td>${season.gamesPlayed?? "--"}</td>
+      <td>${season.goals?? "--"}</td>
+      <td>${season.assists?? "--"}</td>
+      <td>${season.points?? "--"}</td>
+      <td>${season.plusMinus?? "--"}</td>
+      <td>${season.pim?? "--"}</td>
+      <td>${season.gameWinningGoals?? "--"}</td>
+      <td>${season.otGoals?? "--"}</td>
+      <td>${season.powerPlayGoals?? "--"}</td>
+      <td>${season.powerPlayPoints?? "--"}</td>
+      <td>${season.shorthandedGoals?? "--"}</td>
+      <td>${season.shorthandedPoints?? "--"}</td>
+      <td>${season.shootingPctg?? "--"}</td>
+      <td>${season.shots?? "--"}</td>
+      <td>${season.avgToi?? "--"}</td>
+      <td>${season.faceoffWinningPctg?? "--"}</td>
+    </tr>
+    `;
+}).join('');
+  
+  return `
+    <table class="season-table">
+      <thead>
+        <tr>
+          <th>Season</th>
+          <th>Team</th>
+          <th>League</th>
+          <th>GP</th>
+          <th>Goals</th>
+          <th>Ast</th>
+          <th>Pts</th>
+          <th>+/-</th>
+          <th>PIM</th>
+          <th>GWG</th>
+          <th>OtG</th>
+          <th>PPG</th>
+          <th>PPP</th>
+          <th>ShG</th>
+          <th>ShP</th>
+          <th>Shot%</th>
+          <th>Shots</th>
+          <th>AvgTOI</th>
+          <th>FO%</th>
         </tr>
       </thead>
       <tbody>
